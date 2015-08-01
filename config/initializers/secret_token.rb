9c0597c4debe2fda4e647b6c88e4535af32efa73
vrushali waykole <vrushali@amuratech.com>
2015-08-01 18:16:39 +0530

    Initial commit. Created an empty application.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+EmployeeListing::Application.config.secret_token = '3b019eef75dd2934a891aaeab0b032bb04b75a1d617b60777578000cbf8dc7cf4fecb882f248db036a2462c0087ea1618219c12833c4cf2fff36fc8af56b0999'
